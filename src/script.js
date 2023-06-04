ecb59981454a1a9802de3cdc578e99efd7e41fba
l_loizides <lefteris.loizides@mindgeek.com>
2023-06-04 19:18:55 +0300

    update configs

--- src/script.js
+++
@@ -1,7 +0,0 @@
-import { Configuration, OpenAIApi } from 'openai';
-
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-})
-delete config.baseOptions.headers['User-Agent']
-const openai = new OpenAIApi(config);
---
+++ webpack.dev.js
@@ -0,0 +1,56 @@
+import path from 'path'
+import webpack from 'webpack'
+import { fileURLToPath } from 'url';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+  },
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+      },
+    ]
+  },
+  resolve: {
+    extensions: [
+      '*', '.jsx', '.js'
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      // filename: 'popup.html'
+    }),
+    new MiniCssExtractPlugin(),
+    new webpack.ProvidePlugin({
+      "React": "react",
+    }),
+  ]
+}
